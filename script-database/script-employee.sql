dc6c851cc4e35a1859b2af8ce617cb06be09662d
igorfals <igorfeals@gmail.com>
2024-09-24 18:21:14 -0300

    Instalando as dependencias no arquivo 'package.json'

---
+++ script-database/script-employee.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employee (
+	employee_Id uuid PRIMARY KEY NOT null DEFAULT uuid_generate_v4(),
+	name VARCHAR(255) NOT NULL,
+	job_Role VARCHAR(255) NOT NULL,
+	salary NUMERIC(5,2),
+	birth DATE NOT NULL,
+	employee_registration INTEGER NOT NULL
+)

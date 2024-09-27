e894a05f3ed1859de85a3dc6bf13f394424aea9f
igorfals <igorfeals@gmail.com>
2024-09-27 16:33:22 -0300

    Alterando arquivo script-employee.sql

--- sql/script-employee.sql
+++ sql/script-employee.sql
@@ -2,7 +2,7 @@ CREATE TABLE employee (
 	employee_Id uuid PRIMARY KEY NOT null DEFAULT uuid_generate_v4(),
 	name VARCHAR(255) NOT NULL,
 	job_Role VARCHAR(255) NOT NULL,
-	salary NUMERIC(5,2),
+	salary NUMERIC(5,2) NOT NULL,
 	birth DATE NOT NULL,
 	employee_registration INTEGER NOT NULL
 )
